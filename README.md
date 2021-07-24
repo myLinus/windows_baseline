903fddd84c33b05cf97e1a14a90091c8cba2297f
myLinus <jonas.refby@gmail.com>
2021-07-24 17:00:45 +0200

    initial readme, windows.baseline.ps1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# windows_baseline
- 
+# windows_baseline #
+
+This is just an overcomplicated project to play around. :woman_shrugging: :man_shrugging:
+
+
+.help
+Name  | Function
+------------- | -------------
+install  | Install choco and software. 
+update | Update software
+reconfigure |  Overwrites cosmetic configuration files of software.
+
+.pipeline
+- [x] install
+- [X] update
+- [] reconfigure
+
+
+<details>
+<summary>What</summary>
+<p>windows_baseline is a script for installing, updating, and configuring the software I consider a baseline for my Windows-experince.</p>
+</details>
+<details>
+<summary>Why</summary>
+<p>wanted to play around with objects.</p>
+</details>
+<details>
+<summary>License</summary>
+<p>This project is licensed under [Eclipse Public License - v 2.0](https://www.eclipse.org/legal/epl-2.0/)</p>
+</details>
--- windows.baseline.ps1
+++ windows.baseline.ps1
@@ -1,16 +1,18 @@
 <#
 .Synopsis
-   Baseline software and configs for smooth sailings. 
+   Script using choco for baseline software and configs for smooth sailings. 
 .DESCRIPTION
    This script will install, configure & upate baseline software and configs.
 .EXAMPLE
-   ./windows.baseline.ps1 --install --update
+   ./windows.baseline.ps1 -install:$false -upgrade:$true -reconfigure:$true
 .EXAMPLE
-   ./windows.baseline.ps1 --reconfigure
+   ./windows.baseline.ps1
 .INPUTS
-   Inputs to this cmdlet (if any)
+   --install
+   --reconfigure
+   --update
 .OUTPUTS
-   Output from this cmdlet (if any)
+   ¯\_(ツ)_/¯
 .NOTES
    General notes
 .COMPONENT
@@ -23,15 +25,72 @@
    .install and update software packages from $listOfMaslow
    .overwrite config files of packages from $listofMaslow with $minionsOfEdJenner
 #>
-
-$gitprofile = [PSCustomObject]@{
-   username     = 'Kevin'
-   Language = 'PowerShell'
-   State    = 'Texas'
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [bool]
+    $install = $false,
+    [Parameter()]
+    [bool]
+    $update = $true, 
+    [Parameter()]
+    [bool]
+    $reconfigure = $false
+)
+$olympus               = [PSCustomObject]@{
+   software            = 'git,7zip,foxitreader,cherrytree,discord,greenshot,vscode,firefox,powertoys,spotify,telegram,vlc'
+   configs             = $null
+   Scbase              = 'https://raw.githubusercontent.com/myLinus/windows_baseline/main/'
+}
+$bookOfHubbard         = [PSCustomObject]@{
+    chocoNotInstalled  = 'Choco is not installed. Will start installation of Choco#Done! Choco has been installed.'
+    install            = 'Now installing:#Done!'
+    update             = 'Now updating:#Done!'
+    configure          = 'Now configuring:#Done!'
+ }
+ $configPaths          = New-Object 'System.Collections.Generic.Dictionary[String,String]'
+    $configPaths.Add("firefox",'')
+    $configPaths.Add("powershell",[string]$profile)
+$lisftofMaslow = $olympus.software
+        function baselineInstall {
+if (!(choco -v)) {
+    Write-host ([string]$bookOfHubbard.chocoNotInstalled).Split('#')[0]
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Clear-Host
+    Write-host ([string]$bookOfHubbard.chocoNotInstalled).Split('#')[1]
+}
+    foreach ($physiologicalNeed in [array]$lisftofMaslow.Split(',')) {
+        Write-host ([string]($bookOfHubbard.install).Split('#')[0]) ([string]$physiologicalNeed)
+        choco install $physiologicalNeed --yes --no-progress --limitoutput --nocolor
+        Write-host ([string]$bookOfHubbard.install).Split('#')[1]
+        Clear-Host
+        Write-Host 
+    }
+}
+        function basselineUpdate {
+    foreach ($securityAndSafetyNeed in [array]$lisftofMaslow.Split(',')) {
+         Write-host ([string]($bookOfHubbard.install).Split('#')[0]) ([string]$securityAndSafetyNeed)
+        choco update $securityAndSafetyNeed --yes --no-progress --limitoutput --nocolor
+        Write-host ([string]$bookOfHubbard.install).Split('#')[1]
+        }
+}
+    function baselineReconfigure {
+        foreach ($key in $configPaths) {
+            #Invoke-WebRequest -Uri ([string]$olympus.Scbase + )
+            write-host $configPaths.($key)
+        }
+        
 }
 
-$olympus = [PSCustomObject]@{
-   software     = 'git'
-   configs = 'PowerShell'
-   Scbase    = 'Texas'
-} 3z2hoGkNvQ7zrb
+switch ($install) {
+    $true  {baselineInstall}
+    $false {}
+}
+switch ($update) {
+    $true  {basselineUpdate}
+    $false {}
+}
+switch ($reconfigure) {
+    $true  {baselineReconfigure}
+    $false {}
+}
